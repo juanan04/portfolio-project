7c21551c3392a525133e18ac25f5a73508671fa7
juanan04 <ja.aragon23@gmail.com>
2025-03-25 09:42:07 +0100

    removed useless import in ExperienceService

--- backend/src/main/java/ja/portfolio/service/ExperienceService.java
+++ backend/src/main/java/ja/portfolio/service/ExperienceService.java
@@ -5,28 +5,28 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import ja.portfolio.model.Certificate;
 import ja.portfolio.model.Experience;
 import ja.portfolio.repository.ExperienceRepository;
 
 @Service
 public class ExperienceService {
-	
+
 	@Autowired
 	private ExperienceRepository repository;
-	
+
 	public List<Experience> getAllExperiences() {
 		return repository.findAll();
 	}
-	
+
 	public Experience getExperienceById(Long id) throws ExperienceNotFoundException {
-		return repository.findById(id).orElseThrow(()-> new ExperienceNotFoundException("Experience with id " + id + " not found."));
+		return repository.findById(id)
+				.orElseThrow(() -> new ExperienceNotFoundException("Experience with id " + id + " not found."));
 	}
-	
+
 	public Experience saveExperience(Experience experience) {
 		return repository.save(experience);
 	}
-	
+
 	public void deleteExperience(Long id) {
 		repository.deleteById(id);
 	}
